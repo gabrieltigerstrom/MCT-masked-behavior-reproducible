62c3f3540480c015cb7561051439beb555737104
Gabriel Tigerström <gabriel.tigerstrom@gmail.com>
2025-01-27 22:14:55 +0100

    Add reproducible

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MaskedBehaviorReproducible
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MaskedBehaviorReproducible
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
